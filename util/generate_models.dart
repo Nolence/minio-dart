9c93ed1b8c8fa6c47be304090e2d70cfaac27ebb
Nolence <rex.magana@gmail.com>
2021-02-03 09:32:29 -0700

    Updated dependencies to null safe versions

--- util/generate_models.dart
+++ util/generate_models.dart
@@ -28,7 +28,8 @@ const baseUrl = 'https://docs.aws.amazon.com/AmazonS3/latest/API';
 Future<List<String>> getAllModelUrls() async {
   print('Getting Index.');
   final url = '$baseUrl/API_Types_Amazon_Simple_Storage_Service.html';
-  final page = await http.get(url);
+  final uri = Uri.parse(url);
+  final page = await http.get(uri);
   final document = parse(page.body);
   final urls = document.querySelectorAll('.listitem a');
   return urls
@@ -39,7 +40,8 @@ Future<List<String>> getAllModelUrls() async {
 
 Future<String> getModel(String url) async {
   print('Getting: $url.');
-  final page = await http.get(url);
+  final uri = Uri.parse(url);
+  final page = await http.get(uri);
   final document = parse(page.body);
 
   final name = document.querySelector('h1').text;
